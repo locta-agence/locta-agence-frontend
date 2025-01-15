e660c6b64aae2d6fb7d9553f8c43f25b61d22135
Jordan <olivier.jordan21@gmail.com>
2025-01-15 16:10:08 +0100

    mise en place du layout

--- components/layout/footer.vue
+++ components/layout/footer.vue
@@ -1,6 +1,36 @@
 <template>
-  <div class="bg-black text-white p-4">
-    <img src="~/assets/images/logo-white.svg" alt="Logo" class="h-12" />
+  <div class="bg-black text-white flex flex-wrap p-10">
+    <!-- Left Side -->
+    <div class="w-1/3 flex flex-wrap justify-center items-center">
+      <img src="~/assets/images/logo-white.svg" alt="Logo" />
+    </div>
+    <!-- Right Side -->
+    <div class="w-2/3 flex flex-wrap flex-col justify-between items-center">
+      <!-- Menu -->
+      <div class="flex mt-auto text-sm">
+        <!-- Menu -->
+        <ul>
+          <li>Qui-sommes-nous ?</li>
+          <li>Nos projets</li>
+          <li>Nos prestations</li>
+          <li>Articles</li>
+        </ul>
+        <!-- Contact -->
+        <ul class="ml-20">
+          <li>locta.agence@mail.com</li>
+          <li>07 82 38 28 49</li>
+          <li>INSTA</li>
+        </ul>
+      </div>
+      <!-- Legal -->
+      <div class="flex mt-auto gap-6 whitespace-nowrap text-xs">
+        <p>Politique de confidentialité</p>
+        <p>Conditions générales d'utilisation</p>
+        <p>Parametrages des cookies</p>
+        <p>Mentions légales</p>
+      </div>
+    </div>
   </div>
 </template>
 
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
-    <h1 class="text-red-500">Hello World</h1>
+    <h1 class="text-red-500">Accueil</h1>
+    <GallerySection />
+    <QuiSommesNous />
   </div>
 </template>
+
+<script setup>
+import QuiSommesNous from '~/components/sections/QuiSommesNous.vue';
+import GallerySection from '~/components/sections/GallerySection.vue';
+</script>
