26ff9733ada591536a1bd63c6f704e533229c88d
Jordan <olivier.jordan21@gmail.com>
2025-03-19 15:36:27 +0100

    add project section

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <Hero />
+    <ProjectSection />
     <ContactSection />
     <GallerySection />
     <QuiSommesNous />
@@ -9,6 +10,7 @@
 
 <script setup>
 import Hero from '~/components/sections/Hero.vue';
+import ProjectSection from '~/components/sections/ProjectSection.vue';
 import GallerySection from '~/components/sections/GallerySection.vue';
 import QuiSommesNous from '~/components/sections/QuiSommesNous.vue';
 import ContactSection from '~/components/sections/ContactSection.vue';
