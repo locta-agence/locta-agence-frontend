b723d39ab5c944278c677d44a17651a9de644314
sohkey <sohkey.fetch@gmail.com>
2025-03-19 15:12:14 +0100

    feat: update footer

--- components/layout/footer.vue
+++ components/layout/footer.vue
@@ -1,34 +1,49 @@
 <template>
-  <div class="bg-black text-white hidden flex-wrap p-10">
+  <div class="bg-black text-white p-5 w-full flex md:flex-col">
     <!-- Left Side -->
-    <div class="w-1/3 flex flex-wrap justify-center items-center">
-      <img src="~/assets/images/logo-white.svg" alt="Logo" />
-    </div>
-    <!-- Right Side -->
-    <div class="w-2/3 flex flex-wrap flex-col justify-between items-center">
-      <!-- Menu -->
-      <div class="flex mt-auto text-sm">
+    <div class="flex">
+      <div class="w-full md:w-1/4 hidden md:flex justify-center items-center mb-6 md:mb-0">
+        <img class="w-20" src="~/assets/images/logo-white.svg" alt="Logo" />
+      </div>
+
+      <!-- Right Side -->
+      <div class="w-full md:w-3/4 flex flex-col justify-center items-center">
         <!-- Menu -->
-        <ul>
-          <li>Qui-sommes-nous ?</li>
-          <li>Nos projets</li>
-          <li>Nos prestations</li>
-          <li>Articles</li>
-        </ul>
-        <!-- Contact -->
-        <ul class="ml-20">
-          <li>locta.agence@mail.com</li>
-          <li>07 82 38 28 49</li>
-          <li>INSTA</li>
-        </ul>
+        <div
+          class="flex flex-col md:flex-row text-sm w-full justify-center md:justify-between text-center md:text-left gap-6">
+          <!-- Menu -->
+          <ul class="flex flex-col md:flex-row space-y-3 md:space-y-0 md:space-x-5 text-start">
+            <li>Qui-sommes-nous ?</li>
+            <li>Nos projets</li>
+            <li>Nos prestations</li>
+            <li>Articles</li>
+          </ul>
+          <!-- Contact -->
+          <ul class="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-5 text-start">
+            <li>locta.agence@mail.com</li>
+            <li>07 82 38 28 49</li>
+            <li>INSTA</li>
+          </ul>
+          <div class="flex w-full md:hidden flex-col md:flex-row md:space-x-5 text-xs text-start">
+            <p>Politique de confidentialité</p>
+            <p>Conditions générales d'utilisation</p>
+            <p>Paramétrages des cookies</p>
+            <p>Mentions légales</p>
+          </div>
+        </div>
+
+        <!-- Legal -->
       </div>
-      <!-- Legal -->
-      <div class="flex mt-auto gap-6 whitespace-nowrap text-xs">
-        <p>Politique de confidentialité</p>
-        <p>Conditions générales d'utilisation</p>
-        <p>Parametrages des cookies</p>
-        <p>Mentions légales</p>
-      </div>
+    </div>
+    <div class="md:w-1/4 flex md:hidden justify-end items-start mb-6 md:mb-0 ">
+      <img class="w-32 md:w-20" src="~/assets/images/logo-white.svg" alt="Logo" />
+    </div>
+    <div
+      class="hidden md:flex text-neutral-500 flex-col md:flex-row md:justify-center items-center text-xs mt-6 gap-3 w-full text-center md:text-left">
+      <p>Politique de confidentialité</p>
+      <p>Conditions générales d'utilisation</p>
+      <p>Paramétrages des cookies</p>
+      <p>Mentions légales</p>
     </div>
   </div>
 </template>
