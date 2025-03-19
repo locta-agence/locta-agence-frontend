f750402d6871549b9ea906fb328733a98d0debf8
johanpoyet <jojopoy2003@gmail.com>
2025-03-19 15:36:13 +0100

    reparation des degats du rebase

---
+++ pages/admin/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+   
+    </div>
+  </template>
+  
+  <script setup>
+  import Header from '~/components/layout/admin/header.vue';
+  import Footer from '~/components/layout/footer.vue';
+  
+  definePageMeta({
+  layout: "admin",
+});
+  </script>
